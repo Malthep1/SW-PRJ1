67e838cbdaa821bae3ad596783a7f3d4029af3bc
Frederik <frederikpp@hotmail.com>
2021-05-21 17:12:59 +0200

    WIP sensor

---
+++ PRJ1/interrupt.c
@@ -0,0 +1,43 @@
+/*
+ * interrupt.c
+ *
+ * Created: 12-05-2021 12:46:33
+ *  Author: Frederik
+ */ 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+
+volatile unsigned char counter = 0;
+
+ISR (INT0_vect)
+{
+	counter++;
+	EIMSK &= 0b11111100;
+	_delay_ms(1000);
+	EIFR |= 0b00000011;
+	EIMSK |= 0b00000011;
+}
+
+ISR (INT1_vect)
+{
+	counter++;
+	EIMSK &= 0b11111100;
+	_delay_ms(1000);
+	EIFR |= 0b00000011;
+	EIMSK |= 0b00000011;
+}
+
+void initInterrupt()
+{
+	 sei();
+	 DDRD = 0;
+	 EIMSK |= 0b00000011;
+	 EICRA = 0b00001111;
+}
+
+char getCounter()
+{
+	return counter;
+}
---
+++ PRJ1/interrupt.h
@@ -0,0 +1,14 @@
+/*
+ * interrupt.h
+ *
+ * Created: 12-05-2021 12:46:49
+ *  Author: Frederik
+ */ 
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+void initInterrupt();
+char getCounter();
+
+#endif /* INTERRUPT_H_ */
