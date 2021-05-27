8b548956d37aeae6e75d096a09d5a1099a21411a
Martin Stokholm <msldk6a@gmail.com>
2021-05-27 14:40:27 +0200

    flere ændringer

--- DrivingAlgorithm/DrivingAlgorithm/main.c
+++ DrivingAlgorithm/DrivingAlgorithm/main.c
@@ -1,111 +1,14 @@
 #include <avr/io.h>
 #include <stdio.h>
-#include "MotorControl.h"
-#include "LightsControl.h"
-#include "SoundControl.h"
-#include "Sensor_driver.h"
+
 
 int main(void)
 {
     int state = 0;
 	SetupSOMO();
 	initSwitchPort();
-	InitUART(9600, 8);
-	
+	InitUART(9600, 8);	
 	bool carStartBtnPressed = false;
-	
-	while (!carStartBtnPressed)
-	{
-		carStartBtnPressed = switchOn(1);
-	}
-	
-    while (carStartBtnPressed) 
-    {
-		state = getCounter();
-		switch (state)
-		{
-			case 1:
-			//Afspille firstTrack, kør fremad	
-				PlayTrackOne();
-				forward();
-				setSpeed(50);
-			break;
-			
-			case 2:
-			// Kør fremad
-				PlayTrackTwo();
-				forward();
-				setSpeed(75);
-			break;
-			
-			case 3:
-			// Kør fremad op ad bakke
-				PlayTrackTwo();
-				forward();
-				setSpeed(100);
-			break;
-			
-			case 4:
-			// Kør fremad ned ad bakke
-				PlayTrackTwo();
-				forward();
-				setSpeed(25);
-			break;	
-					
-			case 5:
-			// Kør fremad
-				PlayTrackTwo();
-				forward();
-				setSpeed(100);
-			break;
-			
-			case 6:
-			// Stop, begynd at kør baglæns
-				PlayTrackTwo();
-				stop();
-				backward();
-				setSpeed(50);
-			break;	
-								
-			case 7:
-			// Kør baglæns (refleksbrik 6)
-				PlayTrackTwo();
-				backward();
-				setSpeed(75);
-			break;	
-					
-			case 8:
-			// stop, begynd kør fremad (refleksbrik 5)
-				PlayTrackTwo();
-				stop();
-				forward();
-				setSpeed(50);			
-			break;
-			
-			case 9:
-			//kør fremad (reflekrsbrik 5)
-				PlayTrackTwo();
-				forward();
-				setSpeed(100);
-			break;	
-								
-			case 10:
-			// kør fremad (refleksrbik 6)
-				PlayTrackTwo();
-				forward();
-				setSpeed(100);
-			break;
-			
-			case 11:
-			// Stop (refleksbrik 7)
-				PlayTrackThree();
-				stop()
-			break;	
-											
-			default:
-			
-			break;
-		}
-    }
+	drivingAlgorihm();
 }
 
